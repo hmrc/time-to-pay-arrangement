def244ffd6f687a79381d7171621b7bc80b581af
Stephen-Kam <Stephen.Kam@Ten10.com>
2016-11-04 16:27:43 +0000

    SSTTP-447 Merged with master, added tests, refactored test data, Happy Path test passing (#5)
    * initial commit
    
    * SSTTP-460 Initial commit for controllers
    
    * setup integration tests structure
    
    * Added tests, waiting for service to test
    
    * added more integration tests
    
    * Minor test refactoring
    
    * SSTTP-447 merged with master, added tests, Happy Path test passing
    
    * SSTTP-447 Merged with master, added tests, refactored test data
    
    * minor changes
    
    * SSTTP-447 ignoring tests for now until can run tests without services running locally

---
+++ it/uk/gov/hmrc/timetopay/arrangement/CommunicationPreferencesSpec.scala
@@ -0,0 +1,103 @@
+package uk.gov.hmrc.timetopay.arrangement
+
+import uk.gov.hmrc.timetopay.arrangement.itresources._
+import uk.gov.hmrc.timetopay.arrangement.support.{ArrangementActions, IntegrationSpec}
+
+/**
+  * Integration tests for SSTTP-364
+  * POST /ttparrangements
+  */
+
+class CommunicationPreferencesSpec extends IntegrationSpec with ArrangementActions {
+
+  feature("Post Arrangement Details with communication preferences") {
+
+    info("As a consumer of the Arrangement service")
+    info("I want to be able to create a new arrangement")
+
+    ignore("A Welsh user is creating an arrangement with Welsh communication preference") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(welshPreferenceRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("Location").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response and the JSON values should be correct")
+      getArrangementGetResponse.status shouldBe OK
+      val exceptionReason = (getArrangementGetResponse.json \ "letterAndControl" \ "exceptionReason").as[String]
+      exceptionReason should include ("Welsh Preference")
+    }
+
+    ignore("A Welsh user is creating an arrangement with Welsh communication preference") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(welshPreferenceLargePrintRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("Location").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response and the JSON values should be correct")
+      getArrangementGetResponse.status shouldBe OK
+      val exceptionReason = (getArrangementGetResponse.json \ "letterAndControl" \ "exceptionReason").as[String]
+      exceptionReason should include ("Welsh Preference Large Print")
+    }
+
+    ignore("An English user is creating an arrangement with audio indicator communication preference") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(englishAudioIndicatorRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response and the JSON values should be correct")
+      getArrangementGetResponse.status shouldBe OK
+      val exceptionReason = (getArrangementGetResponse.json \ "letterAndControl" \ "exceptionReason").as[String]
+      exceptionReason should include ("Audio Indicator")
+    }
+
+    ignore("An English user is creating an arrangement with large print communication preference") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(englishLargePrintRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response and the JSON values should be correct")
+      getArrangementGetResponse.status shouldBe OK
+      val exceptionReason = (getArrangementGetResponse.json \ "letterAndControl" \ "exceptionReason").as[String]
+      exceptionReason should include ("Large Print")
+    }
+
+    ignore("An English user is creating an arrangement with large print communication preference") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(englishBrailleRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response and the JSON values should be correct")
+      getArrangementGetResponse.status shouldBe OK
+      val exceptionReason = (getArrangementGetResponse.json \ "letterAndControl" \ "exceptionReason").as[String]
+      exceptionReason should include ("Braille")
+    }
+  }
+}
---
+++ it/uk/gov/hmrc/timetopay/arrangement/PostArrangementDetailsSpec.scala
@@ -0,0 +1,69 @@
+package uk.gov.hmrc.timetopay.arrangement
+
+import play.api.libs.json.{JsValue, Json}
+import uk.gov.hmrc.timetopay.arrangement.itresources._
+import uk.gov.hmrc.timetopay.arrangement.support.{ArrangementActions, IntegrationSpec}
+
+/**
+  * Integration tests for SSTTP-364
+  * POST /ttparrangements
+  */
+
+class PostArrangementDetailsSpec extends IntegrationSpec with ArrangementActions {
+
+  feature("Post Arrangement Details") {
+
+    info("As a consumer of the Arrangement service")
+    info("I want to be able to create a new arrangement")
+
+    ignore("An English user is creating an arrangement") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(englishHappyRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier}")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response")
+      getArrangementGetResponse.status shouldBe OK
+      val enforcementAction = (getArrangementGetResponse.json \ "desArrangement" \ "ttpArrangement" \ "enforcementAction").as[String]
+      enforcementAction shouldBe "Distraint"
+    }
+
+    ignore("A Scottish user is creating an arrangement") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(scottishHappyRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response")
+      getArrangementGetResponse.status shouldBe OK
+      val enforcementAction = (getArrangementGetResponse.json \ "desArrangement" \ "ttpArrangement" \ "enforcementAction").as[String]
+      enforcementAction shouldBe "Summary Warrant"
+    }
+
+    ignore("A Welsh user is creating an arrangement") {
+      When("I call POST /ttparrangements")
+      val getArrangementPostResponse = postArrangements(welshHappyRequest)
+
+      Then("I should receive a 201 CREATED response")
+      getArrangementPostResponse.status shouldBe CREATED
+      val locationHeader = getArrangementPostResponse.header("LOCATION").get
+
+      When("I call GET /ttparrangements/{arrangement-identifier")
+      val getArrangementGetResponse = getArrangement(locationHeader)
+
+      Then("I should receive a 200 OK response")
+      val enforcementAction = (getArrangementGetResponse.json \ "desArrangement" \ "ttpArrangement" \ "enforcementAction").as[String]
+      enforcementAction shouldBe "Distraint"
+    }
+  }
+}
