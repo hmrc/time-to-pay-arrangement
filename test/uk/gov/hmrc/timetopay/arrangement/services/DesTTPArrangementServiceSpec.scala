efce20d14159638a7d4393da6c4df5584befaef6
aminmc <aminmc@gmail.com>
2016-11-16 16:20:08 +0000

    test coverage for arrangement

--- test/uk/gov/hmrc/timetopay/arrangement/services/DesTTPArrangementServiceSpec.scala
+++ test/uk/gov/hmrc/timetopay/arrangement/services/DesTTPArrangementServiceSpec.scala
@@ -1,9 +1,14 @@
 package uk.gov.hmrc.timetopay.arrangement.services
 
+import java.time.LocalDate
+
 import org.scalatest.concurrent.ScalaFutures
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 import org.scalatest.prop.TableDrivenPropertyChecks._
+import uk.gov.hmrc.timetopay.arrangement.models.TTPArrangement
 import uk.gov.hmrc.timetopay.arrangement.resources.Taxpayers._
+import uk.gov.hmrc.timetopay.arrangement.resources._
+import uk.gov.hmrc.timetopay.arrangement.modelsFormat._
 
 class DesTTPArrangementServiceSpec extends UnitSpec with WithFakeApplication with ScalaFutures {
 
@@ -27,6 +32,21 @@ class DesTTPArrangementServiceSpec extends UnitSpec with WithFakeApplication wit
         val flag = desTTPArrangementService.enforcementFlag(taxpayer)
         flag shouldBe enforcementFlag
       }
+    }
+
+    "create a des arrangement" in {
+      implicit val arrangement = ttparrangementRequest.as[TTPArrangement]
+      val desArrangement = desTTPArrangementService.create(arrangement).futureValue
+      desArrangement.enforcementAction shouldBe "Distraint"
+      desArrangement.directDebit shouldBe true
+      desArrangement.initials shouldBe "ZZZ"
+      desArrangement.startDate shouldBe LocalDate.parse("2016-09-01")
+      desArrangement.regularPaymentFrequency shouldBe "Monthly"
+      desArrangement.firstPaymentAmount shouldBe "1298.95"
+      desArrangement.saNote shouldBe "SSTTP arrangement created online 1st payment amount of £1298.95 " +
+        "1st payment due date 01/09/2016." +
+        "Regular payment amount £1248.95. Regular payment frequency monthly. " +
+        "Review date 22/08/2017 DDI Ref. 1234567890 PP Ref. 1234567890.TTP letter issued"
     }
   }
 }
