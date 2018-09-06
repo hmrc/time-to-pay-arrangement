9572c4a43dde19f96b959b007228e007154a69b3
AdamShahzad <adam.shahzad@outlook.com>
2018-09-06 10:54:10 +0100

    update library

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -10,7 +10,7 @@ object MicroServiceBuild extends Build with MicroService {
 private object AppDependencies {
   import play.sbt.PlayImport._
 
-  private val scalaTestVersion = "2.2.6"
+  private val scalaTestVersion = "3.0.0"
   private val pegdownVersion = "1.6.0"
 
   private val playReactivemongoVersion = "4.8.0"
@@ -32,7 +32,7 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1",
-        "uk.gov.hmrc" %% "hmrctest" %  "2.3.0",
+        "uk.gov.hmrc" %% "hmrctest" %  "3.0.0",
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % scope,
@@ -51,7 +51,7 @@ private object AppDependencies {
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope,
-        "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % scope,
+        "org.scalamock" %% "scalamock-scalatest-support" % "3.3.0" % scope,
         "org.mockito" % "mockito-core" % "1.10.19"
       )
     }.test
