8521348f519d1f54bf0f156c2b4e02748dc74753
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2024-04-24 13:26:25 +0100

    [KN][OPS-11798] Dependency updates

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -7,7 +7,7 @@ object ScoverageSettings {
     ScoverageKeys.coverageExcludedPackages := "<empty>;.*BuildInfo.*;Reverse.*;app.Routes.*;prod.*;testOnlyDoNotUseInAppConf.*;forms.*;config.*;uk.gov.hmrc.timetopay.arrangement.config.*",
     ScoverageKeys.coverageExcludedFiles := ".*microserviceGlobal.*;.*microserviceWiring.*",
     ScoverageKeys.coverageMinimumStmtTotal := 80,
-    ScoverageKeys.coverageFailOnMinimum := false,
+    ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
 }
